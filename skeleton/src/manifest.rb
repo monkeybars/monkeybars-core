ba08ffe6eb9af284085796b2a5d8c860b3f2a152
Logan Barnett <logustus@gmail.com>
2009-03-15 16:41:36 -0700

    Manifest clears the load path to prevent system/project gem overrides - project gems and libs will be the only ones loaded.

--- skeleton/src/manifest.rb
+++ skeleton/src/manifest.rb
@@ -1,6 +1,7 @@
+$LOAD_PATH.clear #ensure load path is cleared so system gems and libraries are not used (only project gems/libs)
 # Load current and subdirectories in src onto the load path
 $LOAD_PATH << File.dirname(__FILE__)
-Dir.glob(File.expand_path(File.dirname(__FILE__) + "/**")).each do |directory|
+Dir.glob(File.expand_path(File.dirname(__FILE__) + "/**/*")).each do |directory|
   $LOAD_PATH << directory unless directory =~ /\.\w+$/ #File.directory? is broken in current JRuby for dirs inside jars
 end
 
