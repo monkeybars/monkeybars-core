b3daa590bd1a2ecc024eb3535b546cbe1d45643f
Logan Barnett <logustus@gmail.com>
2009-07-01 11:00:49 -0700

    CLI spec now checks for placement of icons.

--- spec/unit/command_line_spec.rb
+++ spec/unit/command_line_spec.rb
@@ -1,32 +1,35 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
 
 describe "Command Line" do
-  before :each do
+  before :all do
     @command = "jruby #{File.join(File.dirname(__FILE__), '..', '..', 'bin', 'monkeybars')} -RI--no-download spec-tmp-proj"
     @temp_project_dir = File.join(File.dirname(__FILE__), '..', '..', 'spec-tmp-proj')
     FileUtils.rm_rf(@temp_project_dir)
+    `#{@command}`
   end
 
-  after :each do
-    #FileUtils.rm_rf(@temp_project_dir)
+  after :all do
+    FileUtils.rm_rf(@temp_project_dir)
   end
 
   it "creates a Rakefile that uses Rawr" do
-    `#{@command}`
-
+    
     File.should be_exist(File.join(@temp_project_dir, 'Rakefile'))
     File.read(File.join(@temp_project_dir, 'Rakefile')).should match(/require 'rawr'/)
   end
 
   it "creates a Rakefile that loads additional Rake tasks from the tasks dir" do
-    puts `#{@command}`
-
     File.should be_exist(File.join(@temp_project_dir, 'Rakefile'))
     File.read(File.join(@temp_project_dir, 'Rakefile')).should match(/Dir\.glob/)
     File.read(File.join(@temp_project_dir, 'Rakefile')).should match(/tasks/)
   end
 
   it "places monkeybars windows and mac icons in the icons dir" do
+    File.should be_exist(File.join(@temp_project_dir, 'icons', 'monkeybars.icns'))
+    File.should be_exist(File.join(@temp_project_dir, 'icons', 'monkeybars.ico'))
+  end
+
+  it "uses the icons in the build_configuration.rb" do
     pending
   end
 end
