91c5f45026d208cb92e8fa98e796d2885e8f6c94
James Britt <james@neurogami.com>
2011-01-19 11:13:29 -0700

    Failed attempts to get example running

--- examples/FlickrBrowser/src/main.rb
+++ examples/FlickrBrowser/src/main.rb
@@ -1,18 +1,20 @@
 require 'pp'
+require 'rbconfig'
+require 'java'
 
 $LOAD_PATH << File.dirname(__FILE__)
 $LOAD_PATH << File.expand_path( File.join( File.dirname(__FILE__), '..', '..', '..', 'lib') )
-require 'foxtrot.jar'
-warn File.dirname(__FILE__)
+$LOAD_PATH << File.expand_path( File.join( File.dirname(__FILE__), 'build', 'classes') )
+$CLASSPATH << 'build/classes'
 warn "$LOAD_PATH = \n#{$LOAD_PATH.pretty_inspect}"
+
+require 'foxtrot.jar'
 
 Dir.glob(File.expand_path(File.dirname(__FILE__) + "/**")).each do |directory|
   $LOAD_PATH << directory unless directory =~ /\.\w+$/ #File.directory? is broken in current JRuby for dirs inside jars
 end
 
 
-require 'rbconfig'
-require 'java'
 require 'manifest'
 
 
